1ba1bce8a95dc33cf545a462151066f97c02e9df
lgeger <87757171+lgeger@users.noreply.github.com>
2024-07-08 11:37:51 +0800

    Update send-email.js

--- functions/api/send-email.js
+++ functions/api/send-email.js
@@ -1,11 +1,24 @@
-export async function onRequestPost(context) {
+export async function onRequestPost(context,env) {
   try {
+    const { request, env } = context;
+
+    // 处理 OPTIONS 请求（预检请求）
+    if (request.method === "OPTIONS") {
+        return new Response(null, {
+        status: 204,
+        headers: corsHeaders
+        });
+    }
       const { from, to, subject, text } = await context.request.json();
 
+      if (!env.RESEND_API_KEY) {
+        throw new Error('RESEND_API_KEY is not set');
+      }
+      
       const response = await fetch('https://api.resend.com/emails', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer re_27NGf74F_5BQnPT6uHiEkczakKx7KNYic',
+            'Authorization': `Bearer ${env.RESEND_API_KEY}`,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
@@ -14,7 +27,7 @@ export async function onRequestPost(context) {
             subject: subject,
             text: text
         })
-    });
+      });
       
       const result = await response.json();
       
